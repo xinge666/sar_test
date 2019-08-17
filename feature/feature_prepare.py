5925ccc932a4384ef86f3a36e41108d5f38bfc05
xinge666 <670915148@qq.com>
2019-08-17 17:38:17 +0800

    update

--- feature/feature_prepare.py
+++ feature/feature_prepare.py
@@ -7,8 +7,15 @@ import utils.file_wav as file_wav
 from utils.data_set_op import Chinese2Pinyin
 from utils.dir_op import get_file_name_ls_from_dir,get_child_dir_ls_from_dir,\
     get_file_dict_for_different_type
+import json
+
 
 class DataSetBase():
+    """
+    子类重写 read_label_file label文件解析函数，
+    或者
+    确定label文件类型后，重写 parse_label_file 解析单个label文件函数 
+    """
     def __init__(self, dataset_dir, label_file = None,label_file_type = 'trn'):
         if dataset_dir[-1]!='/':
             dataset_dir = dataset_dir+'/'
@@ -47,7 +54,7 @@ class DataSetBase():
         pass
 
 class thchs30(DataSetBase):
-    def __init__(self, dataset_dir, label_file = None,label_file_type = 'trn'):
+    def __init__(self, dataset_dir = sys.path[0]+"/../data_thchs30", label_file =None,label_file_type = 'trn'):
         return super().__init__( dataset_dir, label_file,label_file_type)
     def parse_label_files(self,lines):
         return_dic = {}
@@ -58,42 +65,64 @@ class thchs30(DataSetBase):
         return_dic['chinese'] = lines[0]
         return_dic['pinyin'] = lines[1]
         return return_dic
+class aidatatang(DataSetBase):
+    def __init__(self, dataset_dir = sys.path[0]+"/../aidatatang_200zh", 
+            label_file = sys.path[0]+"/../dataset/aidatatang_200zh/transcript/aidatatang_200_zh_transcript.txt",\
+            label_file_type = None):
+        return super().__init__( dataset_dir, label_file,label_file_type)
+
 class aishell(DataSetBase):
-    def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+    def __init__(self, dataset_dir = sys.path[0]+"/../data_aishell", 
+            label_file = sys.path[0]+"/../dataset/data_aishell/transcript/aishell_transcript_v0.8.txt",\
+            label_file_type = None):
+        return super().__init__( dataset_dir, label_file,label_file_type)
+
 
 class ST_CMDS(DataSetBase):
-    def __init__(self, *args, **kwargs):
-        return super().__init__(*args, **kwargs)
+    def __init__(self, dataset_dir, label_file = None,label_file_type = 'txt'):
+        return super().__init__( dataset_dir, label_file,label_file_type)
     def parse_label_files(self,lines):
         return_dic = {}
         return_dic['chinese'] = lines[0]
         return_dic['pinyin'] = Chinese2Pinyin(lines[0])
 
+class primewords(DataSetBase):
+    def __init__(self, dataset_dir = sys.path[0]+"/../primewords_md_2018_set1", \
+            label_file = sys.path[0]+"/../dataset/primewords_md_2018_set1/set1_transcript.json",\
+            label_file_type = None):
+        return super().__init__( dataset_dir, label_file,label_file_type)
+    def parse_label_file(self,labelsplit = ' '):
+        label_dic = {}
+        with open(self.label_file,'r') as f:
+            lines = f.readlines()
+            label_ls = json.loads(lines[0])
+            for label_dic in  label_ls:
+                label_name = label_dic["file"].split('.')[0]
+                label_dic[label_name] = {
+                    'chinese':label_dic["text"],
+                    'pinyin':Chinese2Pinyin(label_dic["text"])
+                }
+            return label_dic
 
-class DatasetPrepare:
-    def __init__(self,dataset_name):
-        if dataset_name=="thchs30":
-            self.dataset_name = "thchs30"
-            self.dataset_root_dir = sys.path[0] +"/../data_thchs30"
-            self.train_dir = self.dataset_root_dir + "/train"
-            self.test_dir = self.dataset_root_dir + "/test"
-        elif dataset_name=="aishell":
-            self.dataset_name = "aishell"
-            self.dataset_root_dir = sys.path[0] +"/../data_aishell/"
-        elif dataset_name=="primewords":
-            self.dataset_name = "thchs30"
-            self.dataset_root_dir = sys.path[0] +"/../thchs30/"
-        elif dataset_name=="ST-CMDS":
-            self.dataset_name = "ST-CMDS"
-            self.dataset_root_dir = sys.path[0] +"/../ST-CMDS-20170001_1-OS/"        
-        elif dataset_name=="train_set":
-            self.dataset_name = "train_set"
-            self.dataset_root_dir = sys.path[0] +"/../train_set"
-    
-    def get_wav_and_label(self):
-        for file in get_file_name_ls_from_dir(self.train_dir):
-            pass
+class MagicData(DataSetBase):
+    def __init__(self, dataset_dir = sys.path[0]+"/../train", \
+            label_file = sys.path[0]+"/../dataset/train/TRANS.txt",\
+            label_file_type = None):
+        return super().__init__( dataset_dir, label_file,label_file_type)
+    def parse_label_file(self,labelsplit = ' '):
+        label_dic = {}
+        with open(self.label_file,'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line_ls = line.split(labelsplit)
+                file_name = line_ls[0].split('.')[0]
+                label = line_ls[-1].strip('\n') 
+                label_dic[file_name] = {
+                    'chinese':label,
+                    'pinyin':Chinese2Pinyin(label)
+                }
+            return label_dic
+
 
 if __name__=="__main__":
     #print(Chinese2Pinyin("你好 我是 顾家新"))
