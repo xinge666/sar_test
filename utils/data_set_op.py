4f6de58cb55d64a5a22f3808f59f353a6024bab6
xinge666 <670915148@qq.com>
2019-08-17 15:57:40 +0800

    update

--- utils/data_set_op.py
+++ utils/data_set_op.py
@@ -3,6 +3,5 @@ translator=Pinyin()
 
 def Chinese2Pinyin(chinese):
     chinese = chinese.replace(" ","")
-    print(chinese)
     one_kw_pinyin=translator.get_pinyin(chinese, ' ',tone_marks='numbers').strip()
     return one_kw_pinyin
