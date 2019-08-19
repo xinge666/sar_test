f8d7dc5309b40b573dbf7bff16f80caf6befb0e4
xinge666 <670915148@qq.com>
2019-08-19 11:17:17 +0800

    update

--- feature/feature_prepare.py
+++ feature/feature_prepare.py
@@ -27,30 +27,30 @@ class DataSetBase():
 
 
     def GetSymbolList(self):
-		'''
-		加载拼音符号列表，用于标记符号
-		返回一个列表list类型变量
-		'''
-		txt_obj=open('dict.txt','r',encoding='UTF-8') # 打开文件并读入
-		txt_text=txt_obj.read()
-		txt_lines=txt_text.split('\n') # 文本分割
-		list_symbol=[] # 初始化符号列表
-		for i in txt_lines:
-			if(i!=''):
-				txt_l=i.split('\t')
-				list_symbol.append(txt_l[0])
-		txt_obj.close()
-		list_symbol.append('_')
-		self.SymbolNum = len(list_symbol)
-		return list_symbol
+        '''
+        加载拼音符号列表，用于标记符号
+        返回一个列表list类型变量
+        '''
+        txt_obj=open('dict.txt','r',encoding='UTF-8') # 打开文件并读入
+        txt_text=txt_obj.read()
+        txt_lines=txt_text.split('\n') # 文本分割
+        list_symbol=[] # 初始化符号列表
+        for i in txt_lines:
+            if(i!=''):
+                txt_l=i.split('\t')
+                list_symbol.append(txt_l[0])
+        txt_obj.close()
+        list_symbol.append('_')
+        self.SymbolNum = len(list_symbol)
+        return list_symbol
 
     def SymbolToNum(self,symbol):
-		'''
-		符号转为数字
-		'''
-		if(symbol != ''):
-			return self.list_symbol.index(symbol)
-		return self.SymbolNum
+        '''
+        符号转为数字
+        '''
+        if(symbol != ''):
+            return self.list_symbol.index(symbol)
+        return self.SymbolNum
 
     def get_child_file_dict(self,root_dir):
         """
